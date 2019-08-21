cb01de167ee2fc16ea30bdfa16ca50264dd53f2d
Daivid Vasconcelos leal <daivid.leal.trd@c6bank.com>
2019-08-21 20:05:37 -0300

    home sign in

--- app/src/main/kotlin/br/com/jujuhealth/features/home/HomeSignFragment.kt
+++ app/src/main/kotlin/br/com/jujuhealth/features/home/HomeSignFragment.kt
@@ -1,8 +1,29 @@
 package br.com.jujuhealth.features.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
 import br.com.jujuhealth.R
+import kotlinx.android.synthetic.main.fragment_home_sign.*
 
 class HomeSignFragment : Fragment(R.layout.fragment_home_sign){
+
+    private lateinit var navController: NavController
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController(view)
+
+        sign_in.setOnClickListener {
+            navController.navigate(R.id.go_to_sign_in)
+        }
+
+        sign_up.setOnClickListener {
+            navController.navigate(R.id.go_to_sign_up)
+        }
+
+    }
 
 }
